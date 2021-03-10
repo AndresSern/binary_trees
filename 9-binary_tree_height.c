b58c0d991ab8e4f3c3f422d0aef74cb9479899a2
AndresSern <andrecamposer@gmail.com>
2021-03-10 21:44:28 +0000

    Putting to the right

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,13 +10,12 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height = 0, right_height = 0;
+	size_t left_height = 0, right_height = 0;
 
 	if (tree == NULL || (tree->right == NULL && tree->left == NULL))
 		return (0);
-
 	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 		return (left_height + 1);
