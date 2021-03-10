b1e38ac264836e5c0906c552c7fa56e0dbf790fa
AndresSern <andrecamposer@gmail.com>
2021-03-10 18:08:25 +0000

    Putting a '*' in the comment about the function and deliting exec

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
-/*
- * binary_tree_insert_left - function that inserts a node
- *                           as the left-child of another node
+/**
+ * binary_tree_insert_left - function that inserts a node as the
+ *                           left-child of another node
  *
- * @pointer: pointer to the node to insert the left-child in
+ * @parent: pointer to the node to insert the left-child in
  * @value:  value to store in the new node
  *
  * Return: return a pointer to the created node,
