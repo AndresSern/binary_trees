0637868b8216062d6bbdcc1f5513d012437dd0d1
andresgfranco <andresgfranco0@gmail.com>
2021-03-11 18:19:33 -0500

    Reorganized library

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,7 @@
 #ifndef BINTREE_H
 #define BINTREE_H
 
+/* Libraries */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -34,7 +35,7 @@ typedef struct binary_tree_s avl_t;
 /* Print Binary Tree */
 void binary_tree_print(const binary_tree_t *);
 
-
+/* Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -43,7 +44,6 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
@@ -55,5 +55,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
 #endif
 
