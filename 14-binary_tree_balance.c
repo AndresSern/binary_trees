83fffe5d2cdcc937b44a74d75e660c5b446017d6
andresgfranco <andresgfranco0@gmail.com>
2021-03-11 18:16:23 -0500

    Correction on betty style

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
@@ -7,11 +8,16 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
 
+/**
+ * binary_tree_height - Function that measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: If tree is NULL, your function must return 0
+**/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0;
